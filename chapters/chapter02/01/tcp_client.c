c4facd0087de5a0bada6eb771bce638094438fe0
keunbum <rkeunbum@gmail.com>
2022-12-05 20:41:49 +0900

    Modify tcp_server.c tcp_client.c

--- chapters/chapter02/01/tcp_client.c
+++ chapters/chapter02/01/tcp_client.c
@@ -9,34 +9,34 @@ void error_handing(const char *);
 
 int main(int argc, char *argv[])
 {
-	int sock;
-	struct sockaddr_in serv_addr;
-	char message[30];
-	int str_len = 0;
+    int sock;
+    struct sockaddr_in serv_addr;
+    char message[30];
+    int str_len = 0;
     int idx = 0;
     int read_len = 0;
-	
-	if (argc != 3)
-	{
-		printf("Usage: %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
 
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock == -1)
-	{
-		error_handing("socket() error");
-	}
+    if (argc != 3)
+    {
+        printf("Usage: %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        error_handing("socket() error");
+    }
 
-	if (connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
-	{
-		error_handing("connect() error");
-	}
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    {
+        error_handing("connect() error");
+    }
 
     while (read_len = read(sock, message + idx++, 1))
     {
@@ -47,14 +47,14 @@ int main(int argc, char *argv[])
         str_len += read_len;
     }
 
-	printf("Message from server: %s\n", message);
+    printf("Message from server: %s\n", message);
     printf("Function read call count: %d\n", str_len);
-	close(sock);
-	return 0;
+    close(sock);
+    return 0;
 }
 
 void error_handing(const char *message)
 {
     perror(message);
-	exit(1);
+    exit(1);
 }
