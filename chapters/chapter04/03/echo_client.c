c306ee01c0725b9957ece9206aa51307cf87a1c7
keunbum <rkeunbum@gmail.com>
2022-12-12 00:50:54 +0900

    Move echo_*.c to my_echo_*.c * Add example code in book

--- chapters/chapter04/03/echo_client.c
+++ chapters/chapter04/03/echo_client.c
@@ -5,92 +5,56 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
-
-#define eprintf(...) fprintf(stderr, __VA_ARGS__)
-
-#define my_func(func, res, ...)                               \
-    do                                                        \
-    {                                                         \
-        if ((res = func(__VA_ARGS__)) == -1)                  \
-        {                                                     \
-            error_handling(#func "(" #__VA_ARGS__ ") error"); \
-        }                                                     \
-    } while (0)
-
-#define my_socket(res, ...) my_func(socket, res, __VA_ARGS__)
-#define my_connect(res, ...) my_func(connect, res, __VA_ARGS__)
-#define my_read(res, ...) my_func(read, res, __VA_ARGS__)
-#define my_write(res, ...) my_func(write, res, __VA_ARGS__)
-#define my_close(res, ...) my_func(close, res, __VA_ARGS__)
-
-#define set_addr(addr, domain, ip, port)           \
-    do                                             \
-    {                                              \
-        memset(&serv_addr, 0, sizeof(addr));       \
-        serv_addr.sin_family = domain;             \
-        serv_addr.sin_addr.s_addr = inet_addr(ip); \
-        serv_addr.sin_port = htons(atoi(port));    \
-    } while (0);
-
-#define BUF_SIZE (64)
-#define TERMINATE_MSG "Q\n"
-
-void error_handling(const char *);
+#define BUF_SIZE 1024
+void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-    int res;
-    int sock;
-    struct sockaddr_in serv_addr;
-    static char msg_buf[BUF_SIZE];
-    static char line[BUF_SIZE];
+	int sock;
+	char message[BUF_SIZE];
+	int str_len;
+	struct sockaddr_in serv_adr;
 
-    if (argc != 3)
-    {
-        printf("Usage: %s <IP> <port>\n", argv[0]);
-        exit(1);
-    }
+	if(argc!=3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	sock=socket(PF_INET, SOCK_STREAM, 0);   
+	if(sock==-1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_adr.sin_port=htons(atoi(argv[2]));
+	
+	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+		error_handling("connect() error!");
+	else
+		puts("Connected...........");
+	
+	while(1) 
+	{
+		fputs("Input message(Q to quit): ", stdout);
+		fgets(message, BUF_SIZE, stdin);
+		
+		if(!strcmp(message,"q\n") || !strcmp(message,"Q\n"))
+			break;
 
-    my_socket(sock, PF_INET, SOCK_STREAM, 0);
-
-    set_addr(serv_addr, AF_INET, argv[1], argv[2]);
-
-    my_connect(res, sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
-    while (1)
-    {
-        int str_len;
-        int read_bytes;
-        int total_read_bytes;
-
-        fputs("Please enter the message you want to send(type Q to exit): ", stdout);
-        if (strcmp(fgets(line, BUF_SIZE, stdin), TERMINATE_MSG) == 0)
-        {
-            my_write(res, sock, "", 0);
-            break;
-        }
-
-        str_len = strlen(line);
-        my_write(res, sock, line, str_len + 1);
-
-        memset(line, 0, sizeof(line));
-        total_read_bytes = 0;
-        while (total_read_bytes < str_len + 1)
-        {
-            my_read(read_bytes, sock, msg_buf, BUF_SIZE - 1);
-            total_read_bytes += read_bytes;
-            strncat(line, msg_buf, read_bytes);
-        }
-        printf("Message from server: %s", line);
-    }
-
-    close(sock);
-    return 0;
+		write(sock, message, strlen(message));
+		str_len=read(sock, message, BUF_SIZE-1);
+		message[str_len]=0;
+		printf("Message from server: %s", message);
+	}
+	
+	close(sock);
+	return 0;
 }
 
-void error_handling(const char *message)
+void error_handling(char *message)
 {
-    perror(message);
-    exit(1);
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
--- chapters/chapter04/03/echo_server.c
+++ chapters/chapter04/03/echo_server.c
@@ -5,112 +5,62 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
-
-#define eprintf(...) fprintf(stderr, __VA_ARGS__)
-
-#define my_func(func, res, ...)                               \
-    do                                                        \
-    {                                                         \
-        if ((res = func(__VA_ARGS__)) == -1)                  \
-        {                                                     \
-            error_handling(#func "(" #__VA_ARGS__ ") error"); \
-        }                                                     \
-    } while (0)
-
-#define my_socket(res, ...) my_func(socket, res, __VA_ARGS__)
-#define my_bind(res, ...) my_func(bind, res, __VA_ARGS__)
-#define my_listen(res, ...) my_func(listen, res, __VA_ARGS__)
-#define my_accept(res, ...) my_func(accept, res, __VA_ARGS__)
-#define my_read(res, ...) my_func(read, res, __VA_ARGS__)
-#define my_write(res, ...) my_func(write, res, __VA_ARGS__)
-#define my_close(res, ...) my_func(close, res, __VA_ARGS__)
-
-#define set_addr(addr, domain, ip, port)        \
-    do                                          \
-    {                                           \
-        memset(&addr, 0, sizeof(addr));         \
-        serv_addr.sin_family = domain;          \
-        serv_addr.sin_addr.s_addr = htonl(ip);  \
-        serv_addr.sin_port = htons(atoi(port)); \
-    } while (0);
-
-#define BUF_SIZE (128)
-#define REQUEST_QUEUE_SIZE 5
-#define CLIENT_ITER_NUM 5
-
-void error_handling(const char *);
+#define BUF_SIZE 1024
+void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-    int res;
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE];
+	int str_len, i;
+	
+	struct sockaddr_in serv_adr;
+	struct sockaddr_in clnt_adr;
+	socklen_t clnt_adr_sz;
+	
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
+	if(serv_sock==-1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
 
-    int serv_sock;
+	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+	
+	if(listen(serv_sock, 5)==-1)
+		error_handling("listen() error");
+	
+	clnt_adr_sz=sizeof(clnt_adr);
 
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
+	for(i=0; i<5; i++)
+	{
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+		if(clnt_sock==-1)
+			error_handling("accept() error");
+		else
+			printf("Connected client %d \n", i+1);
+	
+		while((str_len=read(clnt_sock, message, BUF_SIZE))!=0)
+			write(clnt_sock, message, str_len);
 
-    static char msg_buf[BUF_SIZE];
+		close(clnt_sock);
+	}
 
-    if (argc != 2)
-    {
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
-
-    my_socket(serv_sock, PF_INET, SOCK_STREAM, 0);
-
-    set_addr(serv_addr, AF_INET, INADDR_ANY, argv[1]);
-    my_bind(res, serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
-    my_listen(res, serv_sock, REQUEST_QUEUE_SIZE);
-
-    forn(i, CLIENT_ITER_NUM)
-    {
-        u_char finished;
-        int read_bytes;
-        int total_read_bytes;
-        int clnt_sock;
-        socklen_t clnt_addr_len;
-
-        eprintf("%dth client...\n", i + 1);
-
-        clnt_addr_len = sizeof(clnt_addr);
-        my_accept(clnt_sock, serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_len);
-
-        finished = 0;
-        while (1)
-        {
-            total_read_bytes = 0;
-            do
-            {
-                my_read(read_bytes, clnt_sock, msg_buf, BUF_SIZE - 1);
-                if (read_bytes == 0)
-                {
-                    finished = 1;
-                    break;
-                }
-                total_read_bytes += read_bytes;
-                my_write(res, clnt_sock, msg_buf, read_bytes);
-            } while (msg_buf[read_bytes - 1] != '\0');
-            if (finished)
-            {
-                eprintf("Connection ends with %dth client.\n", i + 1);
-                break;
-            }
-//            eprintf("Message from client: %s", msg_buf);
-//            eprintf("Total read bytes: %d\n", total_read_bytes);
-        }
-
-        my_close(res, clnt_sock);
-    }
-
-    my_close(res, serv_sock);
-    return 0;
+	close(serv_sock);
+	return 0;
 }
 
-void error_handling(const char *message)
+void error_handling(char *message)
 {
-    perror(message);
-    exit(1);
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
